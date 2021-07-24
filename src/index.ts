0bc6bdb7906d094a43cfd2133f05e1b17b22d66a
Dmitry Uvarov <diman_niks@mail.ru>
2021-07-24 15:57:56 +0300

    fix: hz

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,9 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import { v2 as cloudinary } from 'cloudinary';
 
-import swaggerUi from 'swagger-ui-express';
-
 import userRouter from './routes/user';
 import categoryRouter from './routes/category';
 import wordRouter from './routes/word';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const swaggerDocument = require('./doc/api.json');
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -44,8 +39,5 @@ app.use(express.json());
 app.use('/user', userRouter);
 app.use('/category', categoryRouter);
 app.use('/word', wordRouter);
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use((req, res, next) => res.status(404).json({ description: 'Page not found' }));
 
 app.listen(port, () => console.log('server started'));
